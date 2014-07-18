a0e7fc911541b87c29fdca3d90931adf8f44cce8
Stephen Tu <stephen@qadium.com>
2014-07-18 16:52:59 -0700

    libprotobuf now links with libc++ on OS X

--- libprotobuf/run_test.sh
+++ libprotobuf/run_test.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+if [ "`uname`" = "Darwin" ]; then
+    # conda sets MACOSX_DEPLOYMENT_TARGET=10.5, but we need to build with
+    # -stdlib=libc++
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+fi
+echo "Conda testing env:"
+printenv
 protoc schema.proto --cpp_out=.
-g++ -I${PREFIX}/include -L${PREFIX}/lib -o test test.cc schema.pb.cc -lprotobuf
+g++ -v -stdlib=libc++ -I${PREFIX}/include -L${PREFIX}/lib -o test test.cc schema.pb.cc -lprotobuf
 export DYLD_LIBRARY_PATH=${PREFIX}/lib 
 export LD_LIBRARY_PATH=${PREFIX}/lib 
 ./test
