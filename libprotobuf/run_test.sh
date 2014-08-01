3fd6d526a2cdfefd3b93399bc983e73062771126
Stephen Tu <stephen@qadium.com>
2014-08-01 15:55:03 -0700

    fix libprotobuf recipe

--- libprotobuf/run_test.sh
+++ libprotobuf/run_test.sh
@@ -3,11 +3,14 @@ if [ "`uname`" = "Darwin" ]; then
     # conda sets MACOSX_DEPLOYMENT_TARGET=10.5, but we need to build with
     # -stdlib=libc++
     export MACOSX_DEPLOYMENT_TARGET=10.7
+    export CXX="g++ -stdlib=libc++"
+else
+    export CXX=g++
 fi
 echo "Conda testing env:"
 printenv
 protoc schema.proto --cpp_out=.
-g++ -v -stdlib=libc++ -I${PREFIX}/include -L${PREFIX}/lib -o test test.cc schema.pb.cc -lprotobuf
+${CXX} -v -I${PREFIX}/include -L${PREFIX}/lib -o test test.cc schema.pb.cc -lprotobuf
 export DYLD_LIBRARY_PATH=${PREFIX}/lib 
 export LD_LIBRARY_PATH=${PREFIX}/lib 
 ./test
